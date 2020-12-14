ec23574638a6b68bc30f68e8f2c36d46a6da4d85
heqingtong@foxmail.com <heqingtong@foxmail.com>
2020-12-14 17:15:35 +0800

    1

--- src/views/datav/videolist.vue
+++ src/views/datav/videolist.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="videolist">
+  <!-- <div id="videolist">
     <div class="videos">
-      <iframe
+     <iframe
         src="http://172.28.2.200:8083/single/%e5%88%98%e6%bd%ad%e7%ab%99%e5%8f%b3%e7%ba%bf%e6%b4%9e%e5%8f%a3"
         scrolling="no"
       ></iframe>
@@ -24,6 +24,12 @@
         scrolling="no"
       ></iframe>
     </div>
+  </div> -->
+  <div id="videobox">
+    <iframe
+      src="http://172.28.2.200:8083/single/%E6%83%A0%E5%B1%B1%E7%AB%99%E8%B4%A7%E8%BF%90%E8%BD%A6%E9%81%93"
+      scrolling="no"
+    ></iframe>
   </div>
 </template>
 <script>
@@ -38,19 +44,20 @@ export default {
 </script>
 
 <style>
+#videobox {
+  width: 99%;
+  height: 99%;
+}
+#videobox iframe {
+  border: none;
+  height: 100%;
+  width: 100%;
+}
 #videolist {
   margin: 10px 20px;
   display: flex;
   flex-wrap: wrap;
-
   justify-content: space-between;
-  /* // height: 100%;
-  //padding-top: 0px; */
-  /* position: relative;
-  width: calc(50%-25px);
-  margin: 10px;
-  display: flex;
-  */
 }
 #videolist .videos {
   width: 50%;
